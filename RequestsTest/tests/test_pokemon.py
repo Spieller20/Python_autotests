5428127c3cd4b055297881520e3f30545bfece44
Иван <GUF5518@yandex.ru>
2025-09-20 09:59:14 +0500

    test_pokemon.py

--- RequestsTest/tests/test_pokemon.py
+++ RequestsTest/tests/test_pokemon.py
@@ -2,7 +2,7 @@ import requests
 import pytest
 
 URL = 'https://api.pokemonbattle.ru/v2'
-TOKEN = 'f577cdcbd21d0c278dfeae27bf005ca0'
+TOKEN = '{{YOUR_POKEMONBATTLE_TOKEN}}'
 HEADER = {'Content-Type' : 'application/json', 'trainer_token' : TOKEN}
 TRAINER = '38081'
 def test_status_code():
@@ -11,4 +11,5 @@ def test_status_code():
 
 def test_trainer():
     response_get = requests.get(url = f'{URL}/trainers', params = {'trainer_id' : TRAINER})
+
     assert response_get.json()['data'][0]['id'] == '38081'
